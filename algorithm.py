5c3ae1d33b793e242617f2dd77b046444d4266f5
Shayunak <shayanhamidi2000@gmail.com>
2021-12-16 23:09:27 +0330

    The echo-with-extinction algorithm is implemented successfully!

--- algorithm.py
+++ algorithm.py
@@ -7,10 +7,8 @@ from world import log, world, HELLO_MSG
 got_hello_from = []
 echo_round = -1
 isActive = True
-leaderSelected = False
 sub_tree_size = 1
 parent = 0
-got_announce_from = []
 got_echo_message_from = []
 echo_round_wave_member = 0
 
@@ -37,30 +35,26 @@ def create_echo_message_to_parent(tree_size):
 def create_announce_message(leader):
     return f"announce-{leader}"
 
-def broadcast_message_to_neighbors_except_parent(message):
+def broadcast_message_to_neighbors_except_parent(message, the_parent):
     for neighbor in world.neighbors:
-        if neighbor != world.current_node and neighbor != parent:
+        if neighbor != world.current_node and neighbor != the_parent:
             world.send_message(to= neighbor, msg= message)
 
 def start_new_round():
     initialize_parameters_for_each_round()
     log(f"started round {echo_round} with id {echo_round_wave_member}")
-    broadcast_message_to_neighbors_except_parent(message= create_echo_message_to_children())
+    broadcast_message_to_neighbors_except_parent(message= create_echo_message_to_children(), the_parent= parent)
 
 def handle_hello_message(src):
     got_hello_from.append(src)
     if set(got_hello_from) == set(world.neighbors):
         start_new_round()
 
-def announce_leader(leader):
-    global parent
-    parent = world.current_node
-    print(f"The leader is node with physical name {announced_leader}")
-    broadcast_message_to_neighbors_except_parent(message= create_announce_message(leader))
-
 def decide():
     if sub_tree_size == world.network_size:
-        announce_leader(world.current_node)
+        broadcast_message_to_neighbors_except_parent(message= create_announce_message(world.current_node), the_parent= 0)
+        print(f"The leader is {world.current_node}!")
+        sys.exit()
     else:
         start_new_round()
 
@@ -77,7 +71,7 @@ def manage_echo_message_typically(from_node, hit_sub_tree_size):
     global sub_tree_size
     if from_node == parent:
         log(f"message from parent with id {parent} -> broadcasting to neighbors")
-        broadcast_message_to_neighbors_except_parent(message= create_echo_message_to_children())
+        broadcast_message_to_neighbors_except_parent(message= create_echo_message_to_children(), the_parent= parent)
     else:
         got_echo_message_from.append(from_node)
         sub_tree_size = sub_tree_size + hit_sub_tree_size
@@ -108,22 +102,10 @@ def handle_echo_message(from_node, msg, hit_wave_id, hit_round, hit_sub_tree_siz
         log(f"hit by wave with id {hit_wave_id} from round {hit_round} -> adopted the new wave!")
         adopt_new_wave(from_node, hit_wave_id, hit_round)
 
-def check_announce_completeness(announced_leader):
-    if set(got_announce_from) == (set(world.neighbors) - set([world.current_node, parent])):
-        world.send_message(to=parent, msg=create_echo_message_to_parent(tree_size=sub_tree_size))
-
-def handle_announce_message_from_parent(from_node, announced_leader):
-    global leaderSelected, parent
-    print(f"The leader is node with physical name {announced_leader}")
-    leaderSelected = True
-    parent = from_node
-    broadcast_message_to_neighbors_except_parent(message= create_announce_message(announced_leader))
-    check_announce_completeness(announced_leader)
-
-def handle_announce_message_from_children(from_node, announced_leader):
-    got_announce_from.append(from_node)
-    world.send_message(to=from_node, msg= "ack")
-    check_announce_completeness(announced_leader)      
+def handle_announce_message(from_node, announced_leader):
+    broadcast_message_to_neighbors_except_parent(message= create_announce_message(announced_leader), the_parent= from_node)
+    print(f"The leader is {announced_leader}!")
+    sys.exit()
 
 def process_msg(src, msg):
     log(f"message from {src}: {msg}")
@@ -132,15 +114,9 @@ def process_msg(src, msg):
         sys.exit()
     elif msg == HELLO_MSG:
         handle_hello_message(src)
-    elif msg == "ack" and src == parent:
-        sys.exit()
     else:
         splitted_message = msg.split("-")
-        if leaderSelected is False:
-            if len(splitted_message) == 4 and splitted_message[0] == 'echo':
-                handle_echo_message(from_node= src, msg= msg, hit_wave_id= int(splitted_message[1]), hit_round= int(splitted_message[2]), hit_sub_tree_size= int(splitted_message[3]))
-            elif len(splitted_message) == 2 and splitted_message[0] == 'announce':
-                handle_announce_message_from_parent(from_node= src, announced_leader= int(splitted_message[1]))
-        else:
-            if len(splitted_message) == 2 and splitted_message[0] == 'announce':
-                handle_announce_message_from_children(from_node= src, announced_leader= int(splitted_message[1]))
+        if len(splitted_message) == 4 and splitted_message[0] == 'echo':
+            handle_echo_message(from_node= src, msg= msg, hit_wave_id= int(splitted_message[1]), hit_round= int(splitted_message[2]), hit_sub_tree_size= int(splitted_message[3]))
+        elif len(splitted_message) == 2 and splitted_message[0] == 'announce':
+            handle_announce_message(from_node= src, announced_leader= int(splitted_message[1]))
